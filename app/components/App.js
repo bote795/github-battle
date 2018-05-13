a0c7be672056d67f76042247a0f8e103e3d59584
Nicolas Botello <nbotell2@gmail.com>
2018-05-12 22:50:02 -0700

    add nav and home components

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,21 @@
 const React = require('react');
 const Popular = require('./Popular');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Nav = require('./Nav');
+const Home = require("./Home");
 
 class App extends React.Component {
   render() {
     return(
+      <Router>
         <div className="container">
-            <Popular />
+            <Nav />
+            <Route exact path="/" component={Home} />
+            <Route path="/popular" component={Popular}/>
         </div>
+      </Router>  
     )
   }
 }
