213c6c15c7a0d24b04521305226089034ac34b04
Nicolas Botello <nbotell2@gmail.com>
2018-05-15 23:11:03 -0700

    update api js to es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports ={
-    entry: './app/index.js',
+    entry: ['babel-polyfill','./app/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js',
